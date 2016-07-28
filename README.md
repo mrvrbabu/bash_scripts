102b3fcbda4fb1748fd7246bc1e0cb43872cf4b9
root <root@localhost.localdomain>
2016-07-28 12:13:17 -0400

    Adding the xenvm_clone_post_install.sh version 0.1 script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bash_scripts
+
+The xenvm_clone_post_install.sh script will reset the host name and mac address to get host available on the network
---
+++ xenvm_clone_post_install_script.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Author  : Rbabu 
+# Date    : July 2016 
+# Purpose : To configure the vm created using ready made template to necessarey configurations like hostname, network, packages, etc
+
+
+while getopts ':h:m:' option 
+do
+	case $option in
+		h)MYHOSTNAME=$OPTARG;;
+		m)MAC=$OPTARG;;
+		*)echo 
+		  echo "Incorrect switch, Please try executing as sh $0 -h hostname -m macaddress"
+		  echo "Hostname : $MYHOSTNAME Macaddress : $MAC" 
+		  echo 
+		  exit;;
+	esac 
+done
+echo 
+echo "This script can be used to set the hostname and mac address for VMs created using template in xen servers"
+echo 
+echo "Hostname : $MYHOSTNAME Macaddress : $MAC" 
+echo
+#echo "To configure the vm created using ready made template to necessarey configurations like hostname, network, packages, etc"
+#if [ "${HOSTNAME}" == ""  ]
+#then
+#	echo "Please enter the hostname"
+#	break
+#else 
+       #	echo "$HOSTNAME"
+#fi
+
+#if [ "${MAC}" == "" ]
+#then
+#	echo "Please enter the mac address" 
+#	break
+#else 
+      #	echo "$MAC"
+#fi
+echo "Present hostname is $HOSTNAME" 
+echo 
+sleep 2
+echo "Desired hostname is $MYHOSTNAME"
+sleep 2
+echo
+echo "##############Resetting host name from localhost.localdomain to $MYHOSTNAME####################"
+sleep 2
+echo 
+sed -i "/^HOSTNAME=/c\HOSTNAME="$MYHOSTNAME"" /etc/sysconfig/network 
+cat /etc/sysconfig/network
+hostname $MYHOSTNAME 
+echo 
+echo "Host name reset from localhost.localdomain to $MYHOSTNAME"
+echo
+hostname
+echo
+sleep 2
+echo "###################Resetting Mac address#######################" 
+echo
+echo "Present MAC address is : $(grep -i 'HWADDR' /etc/sysconfig/network-scripts/ifcfg-eth0)" 
+echo 
+sleep 2
+echo "Setting new MAC address $MAC"
+cp -rf /etc/sysconfig/network-scripts/ifcfg-eth0 /root/ifcfg-eth0.original
+sed -i "/^HWADDR=/c\HWADDR="$MAC"" /etc/sysconfig/network-scripts/ifcfg-eth0 
+sleep 2
+echo 
+echo "New MAC address set : $(grep -i 'HWADDR' /etc/sysconfig/network-scripts/ifcfg-eth0)" 
+echo 
+echo "Restarting network service"
+echo 
+/etc/init.d/network restart
+echo
+ifconfig eth0  
+
+#EnD 
