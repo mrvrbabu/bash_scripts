b451902e4675180ee8a470f3d63b72b6d5203858
root <root@newcentosvm003.homeoffice.net>
2016-08-10 09:05:05 -0400

    Adding new scripts that would install dhcpd service and configure linux box as router

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # bash_scripts
 
 The xenvm_clone_post_install.sh script will reset the host name and mac address to get host available on the network
+
+
+About "dhcpd-install_configure.sh"
+This script will install and configure the dhcpd server and service on the target server. 
+
+About "linuxbox_as_router_rhel7centos7.sh"
+This script will configure your RHEL7 or CentOS7 hosts as linux router/gateway. 
---
+++ linuxbox_as_router_rhel7centos7.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Author 	: RameshBabu
+# Date		: Aug 2016
+# Purpose	: To configure linux box as router for RHE/Centos - 7
+
+echo 
+echo "To configure a linux box as router on RHEL/CentOS-7"
+echo 
+ip a | grep enp  
+ip a | grep eth  
+echo
+sleep 3 
+read -t 5 -p "Please select the Ethernet nic for outboud traffic i.e to allow outside world access ex:- eth0, eth1, enp123, enp456 : " ethnic0
+echo 
+read -t 5 -p  "Please select the Ethernet nic for inbound traffic i.e the LAN to allow outside world access ex:- eth0, eth1, enp123, enp456 : " ethnic1
+echo $ethnic0
+echo $ethnic1
+
+yum install iptables-services iptables-utils 
+systemctl stop firewalld
+systemctl disable firewalld
+systemctl enable iptables
+systemctl start iptables  
+cat /etc/sysconfig/iptables
+iptables -t nat -nvL
+echo 
+iptables -t nat -A POSTROUTING -o $ethnic0 -j MASQUERADE
+iptables -t nat -nvL
+echo "Saving iptables entries"
+iptables-save
+iptables-save > /etc/sysconfig/iptables
+
+sed -i.bak "/INPUT -j REJECT --reject-with icmp-host-prohibited/ a -A FORWARD -i "$ethnic0" -o "$ethnic1" -m state --state RELATED,ESTABLISHED -j ACCEPT" /etc/sysconfig/iptables 
+sed "/RELATED,ESTABLISHED/ a -A FORWARD -i "$ethnic1" -o "$ethnic0" -j ACCEPT" /etc/sysconfig/iptables 
+systemctl restart iptables 
+
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.d/99-sysctl.conf 
+
+sysctl -p 
+
+echo "Done"
+
+#EnD
